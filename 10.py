7b186e13f90b771a6faa5cc20fa1cc33d62184b3
sundar .s <ssundarajan2006@gmail.com>
2025-08-20 15:48:47 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,10 @@
+<frame set rows>"30,30,40"
+<framename="top"src="index.html"/>
+<framename="content"src""/>
+<framename="footer">
+<frameset col="30,30,*">
+<framename="">src=""/>
+<framename="">src=""/>
+<framename="">src=""/>
+</frame> 
+
